85e12e869426bdac37115d7d9fef9bc90ad0cd41
mrq-lhr <xiehouta@qq.com>
2020-03-13 19:42:55 +0800

    Update Go.py

--- Go.py
+++ Go.py
@@ -17,7 +17,7 @@ print(
 '''
 )
 
-DOWNLOAD=True #下载设置True 安装设置Fals
+DOWNLOAD=True #下载设置True 安装设置False
 if DOWNLOAD:
 	os.system('pip download bs4 lxml xlwt xlrd seaborn -i https://pypi.tuna.tsinghua.edu.cn/simple -d pypi')
 	os.system('pip download torch==1.4.0+cu92 torchvison==0.5.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html -d pypi')
